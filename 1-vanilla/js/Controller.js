2ec541d1e08c63affc354e7ceb18e033843a0844
hjko7 <hj.ko@vogoplay.com>
2026-06-07 21:44:19 +0900

    #1

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,9 +1,20 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, views) {
-    this.store = store;
+  constructor(store, {searchFormView}) {
+    console.log(tag, "constructor");
 
-    // TODO
+    this.store = store;
+    this.searchFormView = searchFormView;
+    this.subscribeViewEvents();
+  }
+
+  // TODO
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", (event) => this.search(event));
+  }
+
+  search(keyword) {
+    console.log(tag,keyword);
   }
 }
